76aa4926b396e8a1d5bf3a6abc87940ab0592545
Rohit Moni <rohitmdxb@gmail.com>
2019-05-14 23:40:22 -0400

    Sample html email sending

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import configparser
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', help="Configuration section to use from the config.ini file")
@@ -16,13 +18,43 @@ else:
 
 recievers = ['rohitmdxb@gmail.com']
 
-subject = "Subject Test"
-message = "test message"
+# Create message container - the correct MIME type is multipart/alternative.
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Test Email with Subject"
+
+# Create the body of the message (a plain-text and an HTML version).
+text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
+html = """\
+<html>
+  <head></head>
+  <body>
+    <h1>Test Email Header</h1>
+    <p>Hi!<br>
+       How are you?<br>
+       Here is the <a href="https://www.python.org">link</a> you wanted.
+    </p>
+  </body>
+</html>
+"""
+
+# Record the MIME types of both parts - text/plain and text/html.
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+msg.attach(part1)
+msg.attach(part2)
 
 try:
     server = smtplib.SMTP_SSL(
         host=config['smtp.host'], port=int(config['smtp.port']))
     server.login(config['smtp.user'], config['smtp.pass'])
-    server.sendmail(config['smtp.user'], recievers, message)
+    failed = server.sendmail(config['smtp.user'], recievers, msg.as_string())
+
+    for email, error in failed.items():
+        print("Error occurred sending to {email}: {error}".format(email=email, error=error))
+
 except smtplib.SMTPException as e:
     print("Error occurred: {error}".format(error=e))
