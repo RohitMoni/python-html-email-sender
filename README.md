27a66012e292097253a233f6ae892429798925f7
Rohit Moni <rohitmdxb@gmail.com>
2019-05-14 23:16:52 -0400

    Email sending functionality. Basic command line argument parsing and config parsing

---
+++ README.md
@@ -0,0 +1,22 @@
+# HTML Email Sender
+
+Simple script to allow for easy e-mail sending.
+Includes html email support with html templates + data passed in to replace data in the template.
+
+## Requirements
+
+Python 3 standard lib
+
+## Usage
+
+Set up the config file. Default section is used whenever there is no value for a key specified in your config section.
+
+Run the python script with the required command line options
+
+```
+python main.py <your_config_section>  
+```
+
+### Examples
+
+`python main.py TEST`
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("test")
+import argparse
+import configparser
+import smtplib
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', help="Configuration section to use from the config.ini file")
+args = parser.parse_args()
+
+configParser = configparser.ConfigParser()
+configParser.read('config.ini')
+
+if args.config:
+    config = configParser[args.config]
+else:
+    config = configParser['DEFAULT']
+
+recievers = ['rohitmdxb@gmail.com']
+
+subject = "Subject Test"
+message = "test message"
+
+try:
+    server = smtplib.SMTP_SSL(
+        host=config['smtp.host'], port=int(config['smtp.port']))
+    server.login(config['smtp.user'], config['smtp.pass'])
+    server.sendmail(config['smtp.user'], recievers, message)
+except smtplib.SMTPException as e:
+    print("Error occurred: {error}".format(error=e))
